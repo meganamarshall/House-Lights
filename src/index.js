4706713cbaa6d9278664e61f2b4005f94dacfc23
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-01 12:01:31 -0800

    adding first loop and function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const windowButtonsNode = document.getElementById('window-buttons');
 const canvasNode = document.getElementById('canvas');
+const transformButtonsNode = document.getElementById('transform-buttons');
 
 const windows = [
     'darkblue',
@@ -15,7 +16,12 @@ for(let index = 0; index < windows.length; index++) {
     windowButtonNode.classList.add('window-button', window);
 
     windowButtonNode.addEventListener('click', function() {
-        paint(window);
+        // make a variable that equals the length of the string
+        // make a loop that limits the length to 8
+        const windowQuantity = document.querySelectorAll('.window-pane').length;
+        if(windowQuantity < 8) {
+            paint(window);
+        }
         console.log(window);
     });
 
@@ -28,5 +34,20 @@ function paint(window) {
     const windowPane = document.createElement('span');
     windowPane.classList.add('window-pane', window);
     canvasNode.appendChild(windowPane);
-
+    
 }
+
+for(let index = 0; index < windows.length; index++) {
+    let light = windows[index];
+    const transformButtonNode = document.createElement('button');
+    transformButtonNode.textContent = light;
+    transformButtonNode.value = light;
+    transformButtonNode.classList.add('transform-button', light);
+
+    transformButtonNode.addEventListener('click', function() {
+        transform(window);
+    });
+    transformButtonsNode.appendChild(transformButtonNode);
+    console.log(transformButtonNode);
+}
+
